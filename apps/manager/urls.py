3d77a1625ecf28d0f16cdd89a1b9955dc4c2badc
Fatema Tuj Johora <tujjohoraa@gmail.com>
2022-12-07 00:20:38 +0600

    updated apps.manager.urls, views

--- apps/manager/urls.py
+++ apps/manager/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from apps.hrm.views import employee_list
 from .views import *
 
 app_name = 'manager'
@@ -8,4 +8,6 @@ urlpatterns = [
     path('guests', guests, name='guests'),
     path('guests-view', guests_view, name='guests-view'),
     path('booking', booking, name='booking'),
+
+    path('employee', employee_list, name="employee"),
 ]
